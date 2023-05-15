a12214bebfee0ea2999346a840bcfe916625dfe0
Cos Kim <junzerokim@gmail.com>
2023-05-15 22:32:12 +0900

    Add: NoteForm

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,14 +1,27 @@
-import { FormEvent, useRef } from 'react';
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { FormEvent, useRef, useState } from 'react';
 import { Button, Col, Form, Row, Stack } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import CrreatableReactSelect from 'react-select/creatable';
+import { NoteData, Tag } from './App';
 
-export function NoteForm() {
+type NoteFormProps = {
+  onSubmit: (data: NoteData) => void;
+};
+
+export function NoteForm({ onSubmit }: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
+
+    onSubmit({
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: [],
+    });
   }
 
   return (
