14b5377d573f5c536f9d6e29d374fbfc7b08e8ef
Cos Kim <junzerokim@gmail.com>
2023-06-17 23:58:09 +0900

    Add: Tag Creation Logic

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -4,13 +4,14 @@ import { NoteForm } from './NoteForm';
 type NewNoteProps = {
   onSubmit: (data: NoteData) => void;
   onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
 };
 
-export function NewNote({ onSubmit }: NewNoteProps) {
+export function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
   return (
     <>
       <h1 className="mb-4">New Note</h1>
-      <NoteForm onSubmit={onSubmit} />
+      <NoteForm onSubmit={onSubmit} onAddTag={onAddTag} />
     </>
   );
 }
